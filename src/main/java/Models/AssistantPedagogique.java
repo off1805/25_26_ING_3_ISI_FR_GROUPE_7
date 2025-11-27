2b56c7f1710bd038880f84cbe2f16ddcbef11424
InnaS._. <innasaida6@gmail.com>
2025-11-27 19:30:44 +0100

    Modification de toutes les classes Utilisateur

--- src/main/java/Models/AssistantPedagogique.java
+++ src/main/java/Models/AssistantPedagogique.java
@@ -1,4 +1,10 @@
 package Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "assistantp")
+
 public class AssistantPedagogique extends Utilisateur{
 }
